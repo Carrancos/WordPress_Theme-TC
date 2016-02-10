db927cd0b84853dc8839bd706337203bcd98a851
lcarranco <larry@carrancomarketing.com>
2016-02-09 21:17:23 -0600

    Implemented ACF to the Agent Login page. This was my first time implementing ACF with a file link.

--- page-333.php
+++ page-333.php
@@ -25,7 +25,7 @@
     </div>
     <div class="investment-photos">
       <div class="col-md-3">
-        <a href="" data-toggle="modal" data-target="#incentiveModalVisaGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1stcase.jpg" alt="1st case"></a>
+        <a href="#" data-toggle="modal" data-target="#incentiveModalVisaGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1stcase.jpg" alt="1st case"></a>
         <!-- Modal -->
         <div class="modal fade" id="incentiveModalVisaGiftCard" role="dialog">
           <div class="modal-dialog">
@@ -52,7 +52,7 @@
         </div>
       </div>
       <div class="col-md-3">
-        <a href="" data-toggle="modal" data-target="#incentiveModalVegasTrip"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/500k.jpg" alt="2nd case"></a>
+        <a href="#" data-toggle="modal" data-target="#incentiveModalVegasTrip"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/500k.jpg" alt="2nd case"></a>
         <!-- Modal -->
         <div class="modal fade" id="incentiveModalVegasTrip" role="dialog">
           <div class="modal-dialog">
@@ -84,7 +84,7 @@
         </div>
       </div>
       <div class="col-md-3">
-        <a href="" data-toggle="modal" data-target="#incentiveModalRitzGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1m.jpg" alt="3rd case"></a>
+        <a href="#" data-toggle="modal" data-target="#incentiveModalRitzGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1m.jpg" alt="3rd case"></a>
         <!-- Modal -->
         <div class="modal fade" id="incentiveModalRitzGiftCard" role="dialog">
           <div class="modal-dialog">
@@ -111,7 +111,7 @@
         </div>
       </div>
       <div class="col-md-3">
-        <a href="" data-toggle="modal" data-target="#incentiveModalSuperbowlTix"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/3m.jpg" alt="4th case"></a>
+        <a href="#" data-toggle="modal" data-target="#incentiveModalSuperbowlTix"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/3m.jpg" alt="4th case"></a>
         <!-- Modal -->
         <div class="modal fade" id="incentiveModalSuperbowlTix" role="dialog">
           <div class="modal-dialog">
@@ -156,88 +156,131 @@
       <div class="col-md-6 col-md-offset-1 ">
         <p>&nbsp;</p>
       </div>
-      
       <div class="clearfix"></div>
   <div class="agentToolbox">
     <div class="row">
       <div class="col-md-6">
-      <h3>Client Presentations</h3>
-      <div class="blackopacity">
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Coming-Soon.jpg" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Coming-Soon.jpg" alt=""></a>
+        <h3>Client Presentations</h3>
+        <div class="blackopacity">
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_1 = get_field('1st_column_-_1st_image_file_link');?>
+              <a href="<?php echo $file_1['url']; ?>" target="_blank">
+                <?php $image_1 = get_field('1st_column_-_1st_image');
+                if( !empty($image_1) ): ?>
+                  <img src="<?php echo $image_1['url']; ?>" alt="<?php echo $image_1['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('1st_column_-_1st_image_title'); ?>
+            </div>
           </div>
-          <div class="col-md-8">
-            <p>Client Mining System</p>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_2 = get_field('1st_column_-_2nd_image_file_link');?>
+              <a href="<?php echo $file_2['url']; ?>" target="_blank">
+                <?php $image_2 = get_field('1st_column_-_2nd_image');
+                if( !empty($image_2) ): ?>
+                  <img src="<?php echo $image_2['url']; ?>" alt="<?php echo $image_2['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('1st_column_-_2nd_image_title'); ?>
+            </div>
           </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Client-Mining-Letter.docx" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Client-Mining-System.jpg" alt=""></a>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_3 = get_field('1st_column_-_3rd_image_file_link');?>
+              <a href="<?php echo $file_3['url']; ?>" target="_blank">
+                <?php $image_3 = get_field('1st_column_-_3rd_image');
+                if( !empty($image_3) ): ?>
+                  <img src="<?php echo $image_3['url']; ?>" alt="<?php echo $image_3['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('1st_column_-_3rd_image_title'); ?>
+            </div>
           </div>
-          <div class="col-md-8">
-            <p>Client Mining Letter</p>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_4 = get_field('1st_column_-_4th_image_file_link');?>
+              <a href="<?php echo $file_4['url']; ?>" target="_blank">
+                <?php $image_4 = get_field('1st_column_-_4th_image');
+                if( !empty($image_4) ): ?>
+                  <img src="<?php echo $image_4['url']; ?>" alt="<?php echo $image_4['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('1st_column_-_4th_image_title'); ?>
+            </div>
           </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Fractionalized-Life-Settlement-Example.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Fractionalized-Life-Settlement-Example.jpg" alt=""></a>
-          </div>
-          <div class="col-md-8">
-            <p>Fractionalized Life Settlement Example<br />
-            <em>one page point of sale piece</em></p>
-          </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Illustration.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Illustration.jpg" alt=""></a>
-          </div>
-          <div class="col-md-8">
-            <p>Illustration<br />
-            <em>one page point of sale piece</em></p>
-          </div>
-        </div>
-
         </div>
       </div>
       <div class="col-md-6">
-      <h3>Brochure</h3>
+        <h3>Brochure</h3>
         <div class="blackopacity">
           <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.jpg" alt=""></a>
+            <div class="col-md-4 clear-pad">
+              <?php $file_5 = get_field('2nd_column_-_1st_image_file_link');?>
+              <a href="<?php echo $file_5['url']; ?>" target="_blank">
+                <?php $image_5 = get_field('2nd_column_-_1st_image');
+                if( !empty($image_5) ): ?>
+                  <img src="<?php echo $image_5['url']; ?>" alt="<?php echo $image_5['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('2nd_column_-_1st_image_title'); ?>
+            </div>
           </div>
-          <div class="col-md-8">
-            <p>Brochure</p>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_6 = get_field('2nd_column_-_2nd_image_file_link');?>
+              <a href="<?php echo $file_6['url']; ?>" target="_blank">
+                <?php $image_6 = get_field('2nd_column_-_2nd_image');
+                if( !empty($image_6) ): ?>
+                  <img src="<?php echo $image_6['url']; ?>" alt="<?php echo $image_6['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('2nd_column_-_2nd_image_title'); ?>
+            </div>
           </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-NQ-App-Packet.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-NQ-App-Packet.jpg" alt=""></a>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_7 = get_field('2nd_column_-_3rd_image_file_link');?>
+              <a href="<?php echo $file_7['url']; ?>" target="_blank">
+                <?php $image_7 = get_field('2nd_column_-_3rd_image');
+                if( !empty($image_7) ): ?>
+                  <img src="<?php echo $image_7['url']; ?>" alt="<?php echo $image_7['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('2nd_column_-_3rd_image_title'); ?>
+            </div>
           </div>
-          <div class="col-md-8">
-            <p>TCS NQ</p>
+          <div class="row brochures">
+            <div class="col-md-4 clear-pad">
+              <?php $file_8 = get_field('2nd_column_-_4th_image_file_link');?>
+              <a href="<?php echo $file_8['url']; ?>" target="_blank">
+                <?php $image_8 = get_field('2nd_column_-_4th_image');
+                if( !empty($image_8) ): ?>
+                  <img src="<?php echo $image_8['url']; ?>" alt="<?php echo $image_8['alt']; ?>" />
+                <?php endif; ?>
+              </a>
+            </div>
+            <div class="col-md-8">
+              <?php the_field('2nd_column_-_4th_image_title'); ?>
+            </div>
           </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Roth-IRA-App-Packet.jpg" alt=""></a>
-          </div>
-          <div class="col-md-8">
-            <p>TCS Roth IRA</p>
-          </div>
-        </div>
-        <div class="row brochures">
-          <div class="col-md-4 clear-pad">
-            <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Traditional-IRA-App-Packet.jpg" alt=""></a>
-          </div>
-          <div class="col-md-8">
-            <p>TCS Traditional IRA</p>
-          </div>
-        </div>
         </div>
       </div>
-
     </div>
   </div>
 </section>
