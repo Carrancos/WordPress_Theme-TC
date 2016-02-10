6d2aeb40d9c92c88f988f9050ba7a5dc50f26429
lcarranco <larry@carrancomarketing.com>
2016-02-09 19:13:05 -0600

    Tried to mess with the php.ini file, but could not change the setting for upload_max_filesize. Fixed the styling of the 2 columns. Specifically, removed the left and right padding of the images and removed the left and right padding of the text when the screen width is below 991px. Implemented the modal to the incentives. Adding styling to the modals.

--- page-333.php
+++ page-333.php
@@ -25,16 +25,123 @@
     </div>
     <div class="investment-photos">
       <div class="col-md-3">
-        <img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1stcase.jpg" alt="1st case">
+        <a href="" data-toggle="modal" data-target="#incentiveModalVisaGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1stcase.jpg" alt="1st case"></a>
+        <!-- Modal -->
+        <div class="modal fade" id="incentiveModalVisaGiftCard" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">January 1, 2015 – December 31, 2015</h4>
+              </div>
+              <div class="modal-body">
+                <div class="col-md-12 clear-pad">
+                  Upon First Case
+                </div>
+                <div class="col-md-12 clear-pad">
+                  $250 Visa Gift Card
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div> 
+          </div>
+        </div>
       </div>
       <div class="col-md-3">
-        <img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/500k.jpg" alt="2nd case">
+        <a href="" data-toggle="modal" data-target="#incentiveModalVegasTrip"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/500k.jpg" alt="2nd case"></a>
+        <!-- Modal -->
+        <div class="modal fade" id="incentiveModalVegasTrip" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">January 1, 2015 – December 31, 2015</h4>
+              </div>
+              <div class="modal-body">
+                <div class="col-md-12 clear-pad">
+                  $500,000 Premium
+                </div>
+                <div class="col-md-12 clear-pad">
+                  Las Vegas Trip for 2
+                  <ul>
+                    <li>includes airfare and transportation to and from hotel</li>
+                    <li>Luxurious accommodations</li>
+                    <li>VIP treatment</li>
+                  </ul>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div> 
+          </div>
+        </div>
       </div>
       <div class="col-md-3">
-        <img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1m.jpg" alt="3rd case">
+        <a href="" data-toggle="modal" data-target="#incentiveModalRitzGiftCard"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/1m.jpg" alt="3rd case"></a>
+        <!-- Modal -->
+        <div class="modal fade" id="incentiveModalRitzGiftCard" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">January 1, 2015 – December 31, 2015</h4>
+              </div>
+              <div class="modal-body">
+                <div class="col-md-12 clear-pad">
+                  $1,000,000 Premium
+                </div>
+                <div class="col-md-12 clear-pad">
+                  $2,500 Ritz-Carlton Gift Card
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div> 
+          </div>
+        </div>
       </div>
       <div class="col-md-3">
-        <img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/3m.jpg" alt="4th case">
+        <a href="" data-toggle="modal" data-target="#incentiveModalSuperbowlTix"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2015/12/3m.jpg" alt="4th case"></a>
+        <!-- Modal -->
+        <div class="modal fade" id="incentiveModalSuperbowlTix" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">January 1, 2015 – December 31, 2015</h4>
+              </div>
+              <div class="modal-body">
+                <div class="col-md-12 clear-pad">
+                  $3,000,000 Premium
+                </div>
+                <div class="col-md-12 clear-pad">
+                  Super Bowl 50 Travel Package
+                  <ul>
+                    <li>2 Flights</li>
+                    <li>2 Game Tickets</li>
+                    <li>3 Night Hotel Stay</li>
+                    <li>VIP events</li>
+                  </ul>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div> 
+          </div>
+        </div>
       </div>
     </div>
     <div class="clearfix"></div>
@@ -57,7 +164,7 @@
       <h3>Client Presentations</h3>
       <div class="blackopacity">
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Coming-Soon.jpg" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Coming-Soon.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -65,7 +172,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Client-Mining-Letter.docx" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Client-Mining-System.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -73,7 +180,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Fractionalized-Life-Settlement-Example.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Fractionalized-Life-Settlement-Example.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -82,7 +189,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Illustration.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/Illustration.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -97,7 +204,7 @@
       <h3>Brochure</h3>
         <div class="blackopacity">
           <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -105,7 +212,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-NQ-App-Packet.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-NQ-App-Packet.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -113,7 +220,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Roth-IRA-App-Packet.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
@@ -121,7 +228,7 @@
           </div>
         </div>
         <div class="row brochures">
-          <div class="col-md-4">
+          <div class="col-md-4 clear-pad">
             <a href="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Brochure-May2015.pdf" target="_blank"><img src="<?php echo site_url('/'); ?>/wp-content/uploads/2016/02/TCS-Traditional-IRA-App-Packet.jpg" alt=""></a>
           </div>
           <div class="col-md-8">
